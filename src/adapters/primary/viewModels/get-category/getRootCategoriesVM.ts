bc01310babe0d358e6881eae4eb0fa19f241f0b4
Piamias Victor <chocowalletdev@proton.me>
2024-10-13 17:05:00 +0200

    img

--- src/adapters/primary/viewModels/get-category/getRootCategoriesVM.ts
+++ src/adapters/primary/viewModels/get-category/getRootCategoriesVM.ts
@@ -39,22 +39,22 @@ export const getImageInMemory = (uuid: UUID): string => {
 
 export const getIconInMemory = (uuid: UUID): string => {
   if (uuid === '19730921-246e-4eec-9a3b-17b49e416c82')
-    return '/_nuxt/assets/bebe.svg'
+    return 'https://i.postimg.cc/kgPc6JKM/baby-svgrepo-com.png'
   if (uuid === '0f4946ae-2e5f-46e8-86a7-fb6d3ae8d75f')
-    return '/_nuxt/assets/dog.svg'
+    return 'https://i.postimg.cc/4dybkmkP/dog.png'
   if (uuid === '0681dc96-411b-4ed5-b457-5c09e7163373')
-    return '/_nuxt/assets/pills.svg'
+    return 'https://i.postimg.cc/k4HNnq7m/pills.png'
   if (uuid === '9869193d-f291-4a74-9d29-b09429b7f81d')
-    return '/_nuxt/assets/nature.svg'
+    return 'https://i.postimg.cc/zvRCv6fR/nature.png'
   if (uuid === 'ed552c28-e8d8-444c-955f-d9ea7803f8fd')
-    return '/_nuxt/assets/ortho.svg'
+    return 'https://i.postimg.cc/YjK1WR4D/ortho.png'
   if (uuid === '7480b66f-d589-42de-a122-3cff0590dd40')
-    return '/_nuxt/assets/dermoBlack.svg'
+    return 'https://i.postimg.cc/667gCQ2C/dermo-Black.png'
   if (uuid === 'e226d36e-1c9e-44e1-8cb0-a70847d35ed7')
-    return '/_nuxt/assets/sante.svg'
+    return 'https://i.postimg.cc/NGWVDwmS/sante.png'
   if (uuid === 'aadb1ea0-3961-46c8-8005-172779c74756')
-    return '/_nuxt/assets/care.svg'
+    return 'https://i.postimg.cc/4NV0FF1d/care.png'
   if (uuid === '03c3ddc9-7616-48df-9bf7-3290da61b23b')
-    return '/_nuxt/assets/promo.svg'
-  return '/_nuxt/assets/category.svg'
+    return 'https://i.postimg.cc/HkthTcR1/promo.png'
+  return 'https://i.postimg.cc/KYvsjchk/category.png'
 }
