e605e099ba42528bbc96cfa026fe2e11c32e750a
Piamias Victor <chocowalletdev@proton.me>
2024-10-17 18:59:49 +0200

    push

--- src/adapters/primary/nuxt/components/FtNavigation.vue
+++ src/adapters/primary/nuxt/components/FtNavigation.vue
@@ -1,12 +1,13 @@
 <template lang="pug">
 div.fixed.w-full.flex.justify-center.z-10.bottom-5.left-0
+    //- pre {{cartQuantity}}
     div.bg-white.rounded-full.p-2.flex.items-center.justify-center.shadow-xl(class='w-[90vw] sm:w-[40vw]')
-        ft-button.w-full.text-contrast.flex.items-center.justify-center(@click="openLike")
+        ft-button.w-full.text-contrast.flex.items-center.justify-center(@click="openLike" :class='color2')
             icon.icon-lg(name="ph:heart-bold")
             span(v-if="likeQuantity && likeQuantity.totalQuantity") {{ likeQuantity.totalQuantity }}
         ft-button.w-full.text-primary11.flex.items-center.justify-center
             icon.icon-lg(name="iconamoon:home-bold")
-        ft-button.w-full.text-contrast.flex.items-center.justify-center(@click="openCart")
+        ft-button.w-full.text-contrast.flex.items-center.justify-center(@click="openCart" :class='color')
             icon.icon-lg(name="akar-icons:shopping-bag")
             span(v-if="cartQuantity && cartQuantity.totalQuantity") {{ cartQuantity.totalQuantity }}
 ft-panel(v-if="cartOpened" @close="closeCart")
@@ -28,6 +29,11 @@ export interface LikeQuantityVM {
   items: HashTable<number>
   totalQuantity: number
 }
+
+const color = ref('text-contrast')
+
+const color2 = ref('text-contrast')
+
 
 const cartOpened = ref(false)
 
@@ -56,6 +62,24 @@ const closeLike = () => {
 // Watch for cart quantity changes
 watchEffect(async () => {
   cartQuantity.value = await getCartQuantityVM(useProductGateway())
+})
+
+watchEffect(async () => {
+  if (cartQuantity.value && cartQuantity.value.totalQuantity && cartQuantity.value.totalQuantity === 0)
+    color.value = 'text-contrast'
+  else if (cartQuantity.value && cartQuantity.value.totalQuantity && cartQuantity.value.totalQuantity !== 0)
+    color.value = 'text-main'
+  else
+    color.value = 'text-contrast'
+})
+
+watchEffect(async () => {
+  if (likeQuantity.value && likeQuantity.value.totalQuantity && likeQuantity.value.totalQuantity === 0)
+    color2.value = 'text-contrast'
+  else if (likeQuantity.value && likeQuantity.value.totalQuantity && likeQuantity.value.totalQuantity !== 0)
+    color2.value = 'text-main'
+  else
+    color2.value = 'text-contrast'
 })
 
 // Watch for like quantity changes
