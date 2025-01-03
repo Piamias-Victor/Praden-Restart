cd520e115af69cd9171406daeac6e1b853d7a356
Piamias Victor <chocowalletdev@proton.me>
2025-01-03 16:38:24 +0100

    name

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,7 +25,7 @@ export default defineNuxtConfig({
 
   app: {
     head: {
-      title: 'Praden MarketPlace',
+      title: 'Pharmacie Agnes Praden - ALES',
       link: [
         {
           rel: 'icon',
