3dd169075add3f09936b8fc3d35e8846c2a525d1
Piamias Victor <chocowalletdev@proton.me>
2025-01-04 17:02:24 +0100

    key

--- gateways/orderGateway.ts
+++ gateways/orderGateway.ts
@@ -8,11 +8,9 @@ import {
 import { StripePaymentGateway } from '@adapters/secondary/payment-gateway/stripePaymentGateway';
 
 export const useOrderGateway = () => {
-  // const STRIPE_SECRET_KEY = "sk_test_51Qc3eEP2AD3Jx99Ykwucclq2og4003IujgMNq2QOqprVnO0rvVbF6gVEBFn2bymcRCerDGytvziuzd4Gu5DoI1hl00wGkQFzFv"
   const {
     public: { STRIPE_SECRET_KEY },
   } = useRuntimeConfig();
-
   const orderGateway = new RealOrderGateway(
     new FakeUUIDGenerator(),
     new StripePaymentGateway(STRIPE_SECRET_KEY),
