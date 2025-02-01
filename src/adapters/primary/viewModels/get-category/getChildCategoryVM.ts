ab85647fd45a9de83cf7e1e1dad7c0f85db07e02
Piamias Victor <chocowalletdev@proton.me>
2025-02-01 11:32:27 +0100

    push

--- src/adapters/primary/viewModels/get-category/getChildCategoryVM.ts
+++ src/adapters/primary/viewModels/get-category/getChildCategoryVM.ts
@@ -30,8 +30,9 @@ export const getChildCategoriesVM = (uuid: UUID): ChildCategoriesVM => {
         name: formatCategoryName(category.name),
         href: `/categories/${category.uuid}`,
         img: getImageInMemory(category.uuid),
+        icon: category.image ?? getIconInMemory(getRootCategoryUuid(category.uuid)),
         // icon: category.image,
-        icon: getIconInMemory(getRootCategoryUuid(category.uuid)),
+        // icon: getIconInMemory(getRootCategoryUuid(category.uuid)),
       };
     }),
   };
